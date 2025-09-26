57415fbcb6f141178ba27d4499081d5690ba0f4e
gitlabs9 <swalesiddiqui505@gmail.com>
2025-09-26 10:15:27 +0530

    Add Finc24.Web

---
+++ Finc24.Web/Finc24.Web/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finc24.Web
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
